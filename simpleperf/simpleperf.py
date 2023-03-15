2cf019d6886c2e79882edeb178bc7dd9aa3e66d1
mePernille <89570696+mePernille@users.noreply.github.com>
2023-03-15 13:45:40 +0100

    update

--- simpleperf/simpleperf.py
+++ simpleperf/simpleperf.py
@@ -3,20 +3,35 @@ import argparse
 from socket import *
 import sys
 
-parser = argparse.ArgumentParser(description="positional arguments", epilog="end of help")
+def check_port(valu):
+    try:
+        value = int(valu)
+    except ValueError:
+        raise argparse.ArgumentTypeError('expected an interger!')
+    if (value <= 0):
+        print('a port must be above zero')
+        sys.exit()
+    return value        
+
+parser = argparse.ArgumentParser(description="A simple iPerf version", epilog="end of help")
 
 # her skal være parser.add_argument('name', )
 parser.add_argument('-s','--server', action='store_true')
-parser.add_argument()
-parser.add_argument()
-parser.add_argument()
+
+
+#parser.add_argument('-l', '--values', help)
+parser.add_argument('-p','--port',type=check_port)
+
+#parser.add_argument()
 
 args = parser.parse_args() # denne MÅ være under add_arguments
 
+
+print("port number: ", args.port)
 #if args.server:
 #    print('The server is on', args.server)
 
-
+'''
 def server(ip,port,format): #format?
     serverPort = 8080 # skal man kunne vælge port selv?
     serverSock = socket(AF_INET,SOCK_STREAM)
@@ -24,4 +39,6 @@ def server(ip,port,format): #format?
         serverSock.bind(('',serverPort))
     except:
         print("Bind failed!")
-        sys.exit()    
+        sys.exit()
+    serverSock.listen()        
+    '''
